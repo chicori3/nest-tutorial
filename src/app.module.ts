05f6909fbef6f11372fded855d529ded285157e1
I C-AN <chicori3@naver.com>
2022-12-21 20:58:54 +0900

    EmailService 작성

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { UsersModule } from './users/users.module';
+import { EmailService } from './email/email.service';
 
 @Module({
 	imports: [UsersModule],
 	controllers: [AppController],
-	providers: [],
+	providers: [EmailService],
 })
 export class AppModule {}
