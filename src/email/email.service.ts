b2cf961f8105742a7b8a726540ea5f96c7bb2333
I C-AN <chicori3@naver.com>
2022-12-21 22:25:46 +0900

    환경변수 값을 사용하도록 수정

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import Mail from 'nodemailer/lib/mailer';
 import * as nodemailer from 'nodemailer';
+import emailConfig from '../config/emailConfig';
+import { ConfigType } from '@nestjs/config';
 
 type EmailOptions = {
 	to: string;
@@ -12,12 +14,14 @@ type EmailOptions = {
 export class EmailService {
 	private transporter: Mail;
 
-	constructor() {
+	constructor(
+		@Inject(emailConfig.KEY) private config: ConfigType<typeof emailConfig>,
+	) {
 		this.transporter = nodemailer.createTransport({
-			service: 'Gmail',
+			service: config.service,
 			auth: {
-				user: 'chicori3@gmail.com',
-				pass: 'mqxnpgspirqabnfm',
+				user: config.auth.user,
+				pass: config.auth.pass,
 			},
 		});
 	}
@@ -25,7 +29,7 @@ export class EmailService {
 		emailAddress: string,
 		signupVerifyToken: string,
 	) {
-		const baseUrl = 'http://localhost:3000';
+		const baseUrl = this.config.baseUrl;
 
 		const url = `${baseUrl}/users/email-verify?signupVerifyToken=${signupVerifyToken}`;
 
