dc14971c55514f735651e87969edd782c9f8d735
I C-AN <chicori3@naver.com>
2022-12-22 23:21:01 +0900

    커스텀 유효성 검사기 작성

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,13 +1,29 @@
-import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
+import {
+	IsEmail,
+	IsString,
+	Matches,
+	MaxLength,
+	MinLength,
+} from 'class-validator';
+import { Transform } from 'class-transformer';
+import { NotIn } from '../../utils/not-in';
 
 export class CreateUserDto {
+	@Transform((params) => params.value.trim())
+	@NotIn('password', {
+		message: 'password는 name과 같은 문자열을 포함할 수 없습니다.',
+	})
 	@IsString()
-	@MinLength(1)
-	@MaxLength(20)
+	@MinLength(2)
+	@MaxLength(30)
 	readonly name: string;
 
+	@IsString()
 	@IsEmail()
+	@MaxLength(60)
 	readonly email: string;
 
+	@IsString()
+	@Matches(/^[a-zA-Z\d!@#$%^&*()]{8,30}$/)
 	readonly password: string;
 }
