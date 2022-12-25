a8ae2ae2f69a4d941b68b5da608e1c3e4631a126
I C-AN <chicori3@naver.com>
2022-12-25 16:21:38 +0900

    typeorm migration 설정

---
+++ src/migrations/1671952664220-CreateUserTable.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class CreateUserTable1671952664220 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
